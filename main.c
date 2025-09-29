d62d8e306a1f57a146c72ecc1781d07d30d238a0
WassimElArche <elarchewassim@gmail.com>
2025-09-29 09:46:44 +0000

    premier ajout

--- main.c
+++ main.c
@@ -1,6 +1,42 @@
 #include <stdio.h>
+#include <stdlib.h>
+#define BASE 2
+
+typedef struct{
+    int *Tdigits;
+    int taille;
+    int Signe;
+
+} BigBinary;
+
+
+BigBinary somme(BigBinary nb1 , BigBinary nb2) {
+
+}
+
+void afficherBigBinary(BigBinary bigBinary) {
+    if (bigBinary.Signe == -1) printf("-");
+    else if (bigBinary.Signe == 0 || bigBinary.taille == 0 ) printf("0");
+    else {
+        for (int i = 0 ; i < bigBinary.taille ; i++) {
+            printf("%d", bigBinary.Tdigits[i]);
+        }
+        printf("\n");
+    }
+}
+
+BigBinary createBigBinary(int taille) {
+    BigBinary bb;
+    bb.Tdigits = malloc(taille * sizeof(int));
+    bb.taille = taille;
+    bb.Signe =0;
+    return bb;
+}
+
 
 int main(){
-printf("testee");
-return 0;
+
+    BigBinary binary;
+    ;
+    return 0;
 }
